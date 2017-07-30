61cd68024a1d3125cadbf354aab70d63c7a2024a
Zhou Junli <zhouhenry@live.com>
2017-07-30 22:59:51 +0800

    add security access setting

--- max_web_select_list_view_column/__manifest__.py
+++ max_web_select_list_view_column/__manifest__.py
@@ -3,11 +3,11 @@
     'name': "Web Select List View Column",
 
     'summary': """
-        To enable user customizing (hide or show) list view columns.
+        To enable user customizing (hide or show) list view columns, also the configuration can be saved and reset.
     """,
 
     'description': """
-        To enable user customizing (hide or show) list view columns.
+        To enable user customizing (hide or show) list view columns, also the configuration can be saved and reset.
     """,
 
     'author': "MAXodoo",
@@ -16,6 +16,7 @@
     'version': '10.0.0.1',
     'depends': ['web'],
     'data': [
+        'security/ir.model.access.csv',
         'views/max_web_select_list_view_column_view.xml',
     ],
     'qweb': ['static/src/xml/list_view_button_view.xml'],
